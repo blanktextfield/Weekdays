2664d1fe14c2e20bde744b3607f7115e0dc42d32
Opilane TTHK <opilane@tthk.ee>
2020-09-15 14:14:27 +0300

    WeekDays

---
+++ WeekDays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WeekDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What day is today?");
+            string day = Console.ReadLine().ToLower();
+            if(day == "monday" || day == "tuesday" || day == "tuesday" || day == "thursday" || day == "friday")
+            {
+                Console.WriteLine($"{day} is a workday");
+            }else if (day == "saturday" || day == "sunday")
+            {
+                Console.WriteLine("it`s weekend");
+            }else
+            {
+                Console.WriteLine("That answer is not a day");
+            }
+        }
+    }
+}
